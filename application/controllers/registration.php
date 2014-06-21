ae751a24aa3a938fd50920377e6fd567048861e4
deepak <deepak@iH8sum3r.(none)>
2014-06-21 22:34:19 +0530

    Changes 0.2

--- application/controllers/registration.php
+++ application/controllers/registration.php
@@ -4,22 +4,21 @@ class Registration extends CI_Controller {
 	public function index() {
 		$this->load->helper('form');
 		$this->load->library('form_validation');
-		$this->form_validation->set_rules('Name', 'Username', 'required');
-		$this->form_validation->set_rules('Password', 'Password', 'required');
-		$this->form_validation->set_rules('Passconf', 'Password Confirmation', 'required');
-		$this->form_validation->set_rules('Email', 'Email', 'required');
+		$this->form_validation->set_rules('name', 'Username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
 		$this->load->helper('url');
 		
 		if ($this->form_validation->run() == FALSE) {
 			$this->load->view('Register/registration.php');
 			$this->load->view('Home/header.php');
 		} else {
-			$this->load->model('register_model');
-			if ($this->input->post('submit')) {
-				$this->register_model->registeruser();
-			}
 			$this->load->view('Register/success.php');
 			$this->load->view('Home/header.php');
+			$this->load->model('register_model');
+			$this->register_model->register_user();
+
 		}
 	}	
 }
--- application/models/register_model.php
+++ application/models/register_model.php
@@ -3,18 +3,15 @@
 * 
 */
 class Register_model extends CI_Model {
-
-	public function Registermodel() {
-		parent::CI_Model();
-	}
-	public function registeruser() {
+	public function register_user() {
 		$this->load->database();
 		$id = $this->input->post('id');
 		$name = $this->input->post('name');
+		$email = $this->input->post('email');
 		$password = $this->input->post('password');
 		$data = array(
 			'name' => $this->input->post('name'),
-			'password' => $this->input->post('age')
+			'password' => $this->input->post('password')
 			);
 		$this->db->insert('register', $data);
 	}
