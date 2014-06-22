8519da0fa61a37346d6aec28705e7d372b752d74
deepak <deepak@iH8sum3r.(none)>
2014-06-22 13:45:14 +0530

    Changes 0.4

--- application/views/Register/success.php
+++ application/views/Register/success.php
@@ -5,22 +5,34 @@
 -->
 <html>
 <head>
-	<title>Register_Done</title>	
+	<title>Register Done</title>	
 	<meta charset="utf-8"> <!-- Character set -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="<?php echo base_url();?>application/views/Register/stylesheets/screen.css" media="screen,projection" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>application/views/Home/stylesheets/mobileView.css" />
 	<script src="<?php echo base_url();?>application/views/Home/js/modernizr.custom.js"></script></head>
 <body>
+<!-- Validations Codeigniter  -->
+<?php echo form_open(); ?>
+<!-- Valdiations Over -->
 	<div class="container register">
 		<header class="header">
-			<h1 class="heading1">Success</h1>
+			<h1 class="heading1">Registered</h1>
 		</header>
-		<div class="register_done">
-			<h1 class="thank">
-				You Have Sucessfully Registered.<br>Please! Confirm Your Email. 
-			</h1>
-		</div>
-	</div>	
+        <div class="s_data shrink">
+            <div class="content1 id_first-slide-text">
+            	<h1 class="heading2">You Have Successfully Registered.<br><br>Please Confirm Your E-mail.</h1>
+            </div>
+            <div class="keepCalm iris">
+            	<img src="<?php echo base_url();?>application/views/Register/stylesheets/tasveer/crown1.png">
+            	<p class="p">Thank You<br>Have A<br>Nice Day</p>
+            </div>
+            <div style="clear:both;">
+           	</div>
+       	</div>
+    </div>
 </body>
+<!-- Script files for mobile view  -->
+<script src="<?php echo base_url();?>application/views/Home/js/classie.js"></script>
+<script src="<?php echo base_url();?>application/views/Home/js/borderMenu.js"></script>
 </html>
