7c1b80a93bbac4693b6770b01e1c5fd6ff0582c5
deepak <deepak@iH8sum3r.(none)>
2014-06-29 04:21:50 +0530

    Changes 1.0

--- application/models/email_model.php
+++ application/models/email_model.php
@@ -7,8 +7,8 @@ class Email_model extends CI_Model {
 			  'protocol' => 'smtp',
 			  'smtp_host' => 'ssl://smtp.gmail.com.',
 			  'smtp_port' => 465,
-			  'smtp_user' => 'deeky.sharma@gmail.com', // change it to yours
-			  'smtp_pass' => '@@DEE123p@k', // change it to yours
+			  'smtp_user' => '', // change it to yours
+			  'smtp_pass' => '', // change it to yours
 			  'mailtype' => 'html',
 			  'charset' => 'iso-8859-1',
 			  'wordwrap' => TRUE
@@ -16,47 +16,11 @@ class Email_model extends CI_Model {
 			
 		$this->load->library('email', $config);
 		$this->email->set_newline("\r\n");
-		$this->email->from('deeky.sharma@gmail.com', "iH8sum3r");
+		$this->email->from('', "iH8sum3r");
 		$this->email->to($email);		
 		$this->email->subject("Yaadein Email Verification");
 		$this->email->message("Dear User,\nPlease click on below URL or paste into your browser to verify your Email Address\n\n http://localhost/~deepak/Souvenir-2.0/index.php/registration/verifyEmailAddress/".$verificationText."\n"."\n\nThanks\nDeePak");
 
 		$this->email->send();
-		
 	}
-
-
-
-
-
- public function sendFeedbackEmail($email) {
-
-                $config = Array(
-                          'protocol' => 'smtp',
-                          'smtp_host' => 'ssl://smtp.gmail.com.',
-                          'smtp_port' => 465,
-                          'smtp_user' => 'deeky.sharma@gmail.com', // change it to yours
-                          'smtp_pass' => '@@DEE123p@k', // change it to yours
-                          'mailtype' => 'html',
-                          'charset' => 'iso-8859-1',
-                          'wordwrap' => TRUE
-                );
-
-                $this->load->library('email', $config);
-                $this->email->set_newline("\r\n");
-                $this->email->from('99satwant@gmail.com', "iH8sum3r");
-                $this->email->to($email);
-                $this->email->subject("Yaadein Feedback Verification");
-                $this->email->message("Your feedback has been successfully recorded");
-
-
-
-                $this->email->send();
-
-        }
-
-
-
-
-
-}
+}	
