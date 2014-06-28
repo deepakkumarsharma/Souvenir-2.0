5a95e4025be3d25e9b68b57cfbf7ec9be487bc3f
deepak <deepak@iH8sum3r.(none)>
2014-06-29 04:29:55 +0530

    Changes 1.1

--- application/views/Sign-in/sign-in.php
+++ application/views/Sign-in/sign-in.php
@@ -5,14 +5,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="<?php echo base_url();?>application/views/Sign-in/stylesheets/screen.css" media="screen,projection" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>application/views/Home/stylesheets/mobileView.css" />
-	<script src="<?php echo base_url();?>application/views/Home/js/modernizr.custom.js"></script></head>
+	<script src="<?php echo base_url();?>application/views/Sign-in/js/sign.js"></script>
+	<script src="<?php echo base_url();?>application/views/Home/js/modernizr.custom.js"></script>
+</head>
 </head>
 <body>
-	<!-- Animated video -->
-	<video class="video-triangle" autoplay="true" loop width="100%" video="100%" style="width:100%, height:100%">
-		<source src="<?php echo base_url();?>application/views/Home/Animation/video.ogv">
-		<source src="<?php echo base_url();?>application/views/Home/Animation/video.mp4">
-	</video>
+<canvas id="canvas" class="canvas"></canvas>
 	<div class="container">
 		<div class="signin">		
 			<h1 class="heading1">Sign In</h1>
