921aaa72723746fff0955979bd4d40e3ef0e3a45
deepak <deepak@iH8sum3r.(none)>
2014-07-11 20:07:15 +0530

    Database

--- Yaadein.sql
+++ Yaadein.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 23, 2014 at 11:25 PM
+-- Generation Time: Jul 11, 2014 at 08:05 PM
 -- Server version: 5.5.37
 -- PHP Version: 5.3.10-1ubuntu3.11
 
@@ -23,6 +23,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `comment` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `register`
 --
 
@@ -34,14 +46,33 @@ CREATE TABLE IF NOT EXISTS `register` (
   `password` varchar(128) NOT NULL,
   `active_status` varchar(2) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
-CREATE TABLE IF NOT EXISTS `feedback` (
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `comment` varchar(500) NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `firstname` varchar(128) NOT NULL,
+  `middlename` varchar(128) NOT NULL,
+  `lastname` varchar(128) NOT NULL,
+  `fathername` varchar(128) NOT NULL,
+  `mothername` varchar(128) NOT NULL,
+  `dob` text NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `mobile` varchar(40) NOT NULL,
+  `postalcode` int(20) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `photo` varchar(100) NOT NULL,
+  `branch` varchar(255) NOT NULL,
+  `classrollno` int(20) NOT NULL,
+  `universityrollno` int(20) NOT NULL,
+  `comment` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
